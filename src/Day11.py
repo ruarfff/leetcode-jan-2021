b6b8c3af54087722e859825a7d1848123b4035f0
Ruairí O'Brien <ruairitobrien@gmail.com>
2021-01-11 20:21:56 +0000

    Another small refactor

--- src/Day11.py
+++ src/Day11.py
@@ -6,17 +6,15 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        m -= 1
-        n -= 1
         i = len(nums1) - 1
 
-        while m >= 0 and n >= 0:
-            if nums1[m] < nums2[n]:
-                nums1[i] = nums2[n]
+        while m >= 1 and n >= 1:
+            if nums1[m - 1] < nums2[n - 1]:
+                nums1[i] = nums2[n - 1]
                 n -= 1
             else:
-                nums1[i] = nums1[m]
+                nums1[i] = nums1[m - 1]
                 m -= 1
             i -= 1
 
-        nums1[: n + 1] = nums2[: n + 1]
+        nums1[:n] = nums2[:n]
