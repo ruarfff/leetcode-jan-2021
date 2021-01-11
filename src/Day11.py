1da0a37a8e4262834b815378ad82f067144a461c
Ruairí O'Brien <ruairitobrien@gmail.com>
2021-01-11 20:17:48 +0000

    small refactor

--- src/Day11.py
+++ src/Day11.py
@@ -5,18 +5,18 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        """        
-        n1 = m - 1
-        n2 = n - 1
+        """
+        m -= 1
+        n -= 1
         i = len(nums1) - 1
 
-        while n1 >= 0 and n2 >= 0:
-            if nums1[n1] < nums2[n2]:
-                nums1[i] = nums2[n2]
-                n2 -= 1
+        while m >= 0 and n >= 0:
+            if nums1[m] < nums2[n]:
+                nums1[i] = nums2[n]
+                n -= 1
             else:
-                nums1[i] = nums1[n1]
-                n1 -= 1
+                nums1[i] = nums1[m]
+                m -= 1
             i -= 1
 
-        nums1[: n2 + 1] = nums2[: n2 + 1]
+        nums1[: n + 1] = nums2[: n + 1]
