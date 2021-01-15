01eb482bd700eb148d44314c8fe787d9579785fd
Ruairí O'Brien <ruairitobrien@gmail.com>
2021-01-15 16:02:25 +0000

    Small refactor

--- src/Day14_GetMaximumInGeneratedArray.py
+++ src/Day14_GetMaximumInGeneratedArray.py
@@ -1,15 +1,18 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
+        l = n + 1
         if n == 0:
             return 0
-        nums = [0] * (n + 1)
+        nums = [0] * l
         nums[1] = 1
+        current_max = 1
 
-        for i in range(1, n):
+        for i in range(1, l):
             x = 2 * i
             if x > 1 and x <= n:
                 nums[x] = nums[i]
             if x + 1 > 1 and x + 1 <= n:
                 nums[x + 1] = nums[i] + nums[i + 1]
+            current_max = max(current_max, nums[i])
 
-        return max(nums)
+        return current_max
