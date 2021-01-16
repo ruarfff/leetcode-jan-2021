791efab86db7247bd47f54501162e2be38f12ec9
Ruairí O'Brien <ruairitobrien@gmail.com>
2021-01-16 15:05:35 +0000

    Further comments

--- src/Day1_CheckArrayFormationThroughConcatenation.py
+++ src/Day1_CheckArrayFormationThroughConcatenation.py
@@ -6,18 +6,22 @@ class Solution:
     # Return true if it is possible to form the array arr from pieces. Otherwise, return false.
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
         # Setup 2 pointers. One to iterate over arr
-        # The other pointer
+        # The other pointer slides along with the pieces
         index = 0
         new_index = index
 
-        # We wont always iterate over every bit of arr. We might jump around a bit so use a while instead of for.
+        # We won't always iterate over every bit of arr so using a while to track index that gets updated in the loop.
         while index < len(arr):
             # Let's look at each piece and see if it matches a part of arr
             for piece in pieces:
                 current_piece_length = len(piece)
-                # Check if the pice matches a section of arr from the current index.
+                # Check if the piece matches a section of arr from the current index.
                 # Example. if i = 0, arr = [1,2,3], piece = [1,2] then we would have a match and new_index would jump to 2 i.e. 0 + len(piece)
-                if piece == arr[index : index + current_piece_length]:
+                # Checking piece[0] == arr[index] is just a performance optimisation so we don't always check the full piece unnecessarily
+                if (
+                    piece[0] == arr[index]
+                    and piece == arr[index : index + current_piece_length]
+                ):
                     new_index += current_piece_length
                     # We don't want to count this piece again so remove it
                     pieces.remove(piece)
