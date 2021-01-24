d1e2c1f6d98cc7371175f6be4fc1e32049d7a052
Ruairí O'Brien <ruairitobrien@gmail.com>
2021-01-24 13:23:57 +0000

    Aded merge version of Day24

--- src/Day24_MergeKSortedLists.py
+++ src/Day24_MergeKSortedLists.py
@@ -3,6 +3,22 @@ from .util import ListNode
 from queue import PriorityQueue
 
 
+def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
+    head = ListNode()
+    current = head
+    while l1 and l2:
+        if l1.val <= l2.val:
+            current.next = l1
+            l1 = l1.next
+        else:
+            current.next = l2
+            l2 = l1
+            l1 = current.next.next
+        current = current.next
+    current.next = l2 if l2 else l1
+    return head.next
+
+
 class Solution:
     def build_queue(self, lists: List[ListNode]) -> PriorityQueue:
         q = PriorityQueue()
@@ -11,7 +27,7 @@ class Solution:
                 q.put((l.val, l), False)
         return q
 
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+    def mergeKLists2(self, lists: List[ListNode]) -> ListNode:
         head = ListNode()
         current = head
         q = self.build_queue(lists)
@@ -25,3 +41,16 @@ class Solution:
                 q.put((next_node.val, next_node), False)
 
         return head.next
+
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        k = len(lists)
+        if k == 0:
+            return None
+
+        step = 1
+        while step < k:
+            for i in range(0, k - step, step * 2):
+                lists[i] = mergeTwoLists(lists[i], lists[i + step])
+            step *= 2
+
+        return lists[0]
--- src/Day24_MergeKSortedLists_test.py
+++ src/Day24_MergeKSortedLists_test.py
@@ -11,3 +11,10 @@ s = Solution()
 )
 def test_merge_k_lists(lists, expected):
     assert toList(s.mergeKLists(list(map(lambda x: toListNode(x), lists)))) == expected
+
+@pytest.mark.parametrize(
+    "lists,expected",
+    [([[1, 4, 5], [1, 3, 4], [2, 6]], [1, 1, 2, 3, 4, 4, 5, 6]), ([], []), ([[]], [])],
+)
+def test_merge_k_lists2(lists, expected):
+    assert toList(s.mergeKLists2(list(map(lambda x: toListNode(x), lists)))) == expected
